984c77e4f8d28ad0dc896a2c504cbb00e8a1286f
Thrilok kumar <thrilok2000@gmail.com>
2026-05-16 00:30:41 +0400

    update payload IGP-129

--- contracts/payloads/IGP129/PayloadIGP129.sol
+++ contracts/payloads/IGP129/PayloadIGP129.sol
@@ -4,15 +4,20 @@ pragma experimental ABIEncoderV2;
 
 import {PayloadIGPPriceHelpers} from "../common/pricehelpers.sol";
 
-/// @notice IGP129: Treasury withdrawal to Team Multisig. // Token and Amount needs to be filled before sending IGP129.
+/// @notice IGP129: Withdraw the Treasury's full iETHv2 balance to Team
+///         Multisig (as iETHv2) and redeem the Treasury's full fGHO position
+///         to GHO, sending the GHO to Team Multisig.
 contract PayloadIGP129 is PayloadIGPPriceHelpers {
     uint256 public constant PROPOSAL_ID = 129;
 
     function execute() public virtual override {
         super.execute();
 
-        // Action 1: Withdraw funds from Treasury to Team Multisig
+        // Action 1: Withdraw full iETHv2 balance from Treasury to Team Multisig.
         action1();
+
+        // Action 2: Redeem Treasury's full fGHO position to GHO and send GHO to Team Multisig.
+        action2();
     }
 
     function verifyProposal() public view override {}
@@ -27,22 +32,34 @@ contract PayloadIGP129 is PayloadIGPPriceHelpers {
      * |__________________________________
      */
 
-    /// @notice Action 1: Withdraw funds from Treasury to Team Multisig
+    /// @notice Action 1: Withdraw full iETHv2 balance from Treasury to Team Multisig.
     function action1() internal isActionSkippable(1) {
-        // TODO: fill token and amount before finalizing IGP129.
-        address token_ = address(0);
-        uint256 amount_ = 0;
-        require(token_ != address(0), "withdraw-token-not-set");
-        require(amount_ != 0, "withdraw-amount-not-set");
-
         string[] memory targets_ = new string[](1);
         bytes[] memory encodedSpells_ = new bytes[](1);
 
         targets_[0] = "BASIC-A";
         encodedSpells_[0] = abi.encodeWithSignature(
             "withdraw(address,uint256,address,uint256,uint256)",
-            token_,
-            amount_,
+            address(IETHV2),
+            type(uint256).max,
+            TEAM_MULTISIG,
+            0,
+            0
+        );
+
+        TREASURY.cast(targets_, encodedSpells_, address(this));
+    }
+
+    /// @notice Action 2: Redeem Treasury's full fGHO position to GHO and send GHO to Team Multisig.
+    function action2() internal isActionSkippable(2) {
+        string[] memory targets_ = new string[](1);
+        bytes[] memory encodedSpells_ = new bytes[](1);
+
+        targets_[0] = "BASIC-D-V2";
+        encodedSpells_[0] = abi.encodeWithSignature(
+            "redeem(address,uint256,address,uint256,uint256)",
+            F_GHO_ADDRESS,
+            type(uint256).max,
             TEAM_MULTISIG,
             0,
             0
